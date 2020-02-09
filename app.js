f3ad5a916ce25244013e7b52e5079235b6096747
Sonu sharma <53683626+sonu3323@users.noreply.github.com>
2020-02-09 09:54:15 +0530

    git ignore procfile and update port

--- app.js
+++ app.js
@@ -196,7 +196,19 @@ app.get("/about", function (req, res) {
 });
 
 
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+
+
+
+
 //Server connections
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
